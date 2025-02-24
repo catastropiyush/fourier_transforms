9dae68a8c33f59e9582cc99bc3e8f55c6ea71394
HimanshuSingh-all <87076645+HimanshuSingh-all@users.noreply.github.com>
2025-02-24 21:37:38 +0530

    Added convolve1d, it ain't much but it's honest work, will later compare it with fft in 1d to see what sort of boundary conditions does a convolution with fft does

--- convole1d.py
+++ convole1d.py
@@ -9,18 +9,27 @@ def convolve_1d(kernel, reference, boundary = 'none'):
         for i in range(len(reference)-len(kernel)+1):
             convolution.append( np.sum(flipped*reference[i:i+len(kernel)]) )
     elif boundary == 'zero_pad':
-        reference = np.concatenate( )
+        ## less memory efficient, but fine for a beginner like me 
+        ## might want to look into how they implement the convolution in the space 
+        ## in these public libraries
+        reference = np.hstack( (np.zeros(len(kernel) -1 ),  reference, np.zeros(len(kernel) -1 ) )  ) 
+        for i in range(len(reference) - len(kernel) -1):
+            convolution.append( np.sum(flipped*reference[i:i+len(kernel)]) )
+    elif boundary == 'periodic':
         for i in range(len(reference)):
             convolution.append( np.sum(flipped*reference[i:i+len(kernel)]) )
     return convolution
 
-kernel = [0, 1, 2]
-kernel = np.array(kernel)
-print(f'Kernel is: {kernel}, shape: {kernel.shape}.')
+if __name__ == '__main__':
 
-reference = [1, 3 ,4 ,4 ,5 ,5, 6, 7, 0]
-reference = np.array(reference)
-print(f'reference is: {reference}, shape: {reference.shape}.')
+    boundary = 'zero_pad'
+    kernel = [0, 1, 2]
+    kernel = np.array(kernel)
+    print(f'Kernel is: {kernel}, shape: {kernel.shape}.')
 
-convolution_result = convolve_1d(kernel, reference, None)
-print(f'The result of the convolution is: {convolution_result}, shape = {len(convolution_result)}')
+    reference = [1, 3 ,4 ,4 ,5 ,5, 6, 7, 0]
+    reference = np.array(reference)
+    print(f'reference is: {reference}, shape: {reference.shape}.')
+
+    convolution_result = convolve_1d(kernel, reference, boundary)
+    print(f'The result of the convolution is: {convolution_result}, shape = {len(convolution_result)}')
--- oned.py
+++ oned.py
@@ -1,11 +1,15 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-signal = np.arange(1,21)
+range = np.linspace(1,21, 1000)
+signal = 5 + 3* np.cos(4* np.pi* range) - 2* np.cos(2* np.pi* range)
 noise = np.random.normal(0, 0.5, size = signal.shape)
-
 observed_sig =  signal + noise
 
-def mydft(signal):
+plt.style.use('ggplot')
+plt.plot(range, signal)
+plt.show()
+def mydft_badway(signal:np.ndarray):
     """
     Compute the Discrete Fourier Transform(DFT) of of the given 1-D signal
     ___
@@ -13,5 +17,10 @@ def mydft(signal):
     * signal (1D numpy array): Signal to calculate the fourier transform for.
 
     Output:
-    * DFT: Compute the  
+    * DFT: The Computed DFT array  
     """
+    # assert type(signal)
+    assert len(signal.shape) ==1, "The signal is not one dimensional! I have been betrayed!"
+    N = signal.shape[0]
+    w = np.exp( -2*1j*np.pi/N)
+    
